2938f0954ed5e70762d18a81f30cd97b6f192b38
Hyeonjae.kim <hyeonjae.dev@gmail.com>
2023-06-29 11:28:02 +0900

    feat: Add JWT verify & File upload

--- work.py
+++ work.py
@@ -1,12 +1,41 @@
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request, abort
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
+from werkzeug.utils import secure_filename
+from google.cloud import storage
+import os
+import uuid
 
 load_dotenv()
 
 app = Flask(__name__)
 
+def bucket_filename(user_id, filename):
+    return str(user_id) + '/' + str(uuid.uuid4()) + '_' + secure_filename(filename)
+
+def upload_file(bucket, user_id, file):
+    filename = bucket_filename(user_id, file.filename)
+    blob = bucket.blob(filename)
+    blob.upload_from_filename(file)
+    return blob.public_url
+
 @app.route('/work', methods=['POST'])
+@jwt_required()
 def create_work():
+    user_id = get_jwt_identity()
+    input_image = request.files['input']
+    mask_image = request.files['mask']
+    prompt = request.form['prompt']
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(os.getenv('bucket_name'))
+
+    if input_image and mask_image:
+        input_public_url = upload_file(bucket, user_id, input_image)
+        mask_public_url = upload_file(bucket, user_id, mask_image)
+    else:
+        abort(400)
+
     return
 
 @app.route('/work', methods=['GET'])
